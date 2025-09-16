2c57d7c680f95264fc24f4f04b8c1fda4b5ebb04
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-09-16 14:37:02 -0400

    For loop exercise

---
+++ src/ForLoopPractice.java
@@ -0,0 +1,19 @@
+public class ForLoopPractice {
+    public static void main(String[] args) {
+
+//        First, let's do it with a while loop
+        int n = 0;
+        while (n <= 10) {
+            System.out.println(n);
+            n++;
+
+        }
+        System.out.println();
+
+
+//        Same thing, but with a FOR Loop
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
