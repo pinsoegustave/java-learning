e9920e03f429bb7fd4bc849b1135be74bd2c4972
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-09-18 13:27:10 -0400

    Displaying all days of the year

---
+++ src/SelfTest.java
@@ -0,0 +1,12 @@
+public class SelfTest {
+    public static void main(String[] args) {
+
+        // Display days and months of 2025
+
+        for (int months = 1; months <= 12; months++) {
+            for (int days = 1; days <= 31; days++) {
+                System.out.println(days +" / "+ months+" / "+ "2025");
+            }
+        }
+    }
+}
