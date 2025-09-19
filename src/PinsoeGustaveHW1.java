de7d25e7d379a654fbdf16de9dc3962bd7794219
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-09-19 11:49:03 -0400

    Uncomment codes before submission

--- src/PinsoeGustaveHW1.java
+++ src/PinsoeGustaveHW1.java
@@ -5,65 +5,65 @@ public class PinsoeGustaveHW1 {
 
 //        Question 1:
 //        Variable declaration
-//        String firstName;
-//        String lastName;
-//        int age;
-//        String favoriteFood;
-//        double costOfSoda;
-//        boolean isWinter;
-//
-////        Variable initialization
-//        firstName = "Pinsoe";
-//        lastName = "Gustave";
-//        age = 19;
-//        favoriteFood = "Burger";
-//        costOfSoda = 8.99;
-//        isWinter = false;
-//
-////        Printing out the message
-//        System.out.print("My name is "+ firstName+" "+lastName +"and I am "+age+ " years old \n");
-//        System.out.println(favoriteFood+ " is my favorite food.");
-//        System.out.println("It is "+ isWinter +" that is winter");Writing & analysis
+        String firstName;
+        String lastName;
+        int age;
+        String favoriteFood;
+        double costOfSoda;
+        boolean isWinter;
+
+//        Variable initialization
+        firstName = "Pinsoe";
+        lastName = "Gustave";
+        age = 19;
+        favoriteFood = "Burger";
+        costOfSoda = 8.99;
+        isWinter = false;
+
+//        Printing out the message
+        System.out.print("My name is "+ firstName+" "+lastName +"and I am "+age+ " years old \n");
+        System.out.println(favoriteFood+ " is my favorite food.");
+        System.out.println("It is "+ isWinter +" that is winter");
 
 //        Question 2:
-//        int a = 5;
-//
-//        System.out.println(a);
-//        a += 24;
-//        System.out.println(a);
-//        a /= 4;
-//        System.out.println(a);
-//        a *= 15;
-//        System.out.println(a);
-//        a -= 7;
-//        System.out.println(a);
-//
-////        Question 3:
+        int a = 5;
+
+        System.out.println(a);
+        a += 24;
+        System.out.println(a);
+        a /= 4;
+        System.out.println(a);
+        a *= 15;
+        System.out.println(a);
+        a -= 7;
+        System.out.println(a);
+
+//        Question 3:
 //        Scanner s = new Scanner(System.in);
 //        System.out.println("Enter your firstName: ");
 //        String firstName = s.nextLine();
 //        System.out.println("Enter your lastName: ");
 //        String lastName = s.nextLine();
-//
-//        System.out.println("First name: " + firstName);
-//        System.out.println("Last name: "+ lastName);
-//        System.out.println("Full Name "+ firstName +" "+ lastName);
+
+        System.out.println("First name: " + firstName);
+        System.out.println("Last name: "+ lastName);
+        System.out.println("Full Name "+ firstName +" "+ lastName);
 
 //        Question 4:
-//        Scanner p = new Scanner(System.in);
-//        System.out.println("Enter player name: ");
-//        String playerName = p.nextLine();
-//        System.out.println("Enter total games played: ");
-//        String totalGamesPlayed = p.nextLine();
-//        int totalGames = Integer.parseInt(totalGamesPlayed);
-//        System.out.println("Enter the total goals: ");
-//        String totalGoals = p.nextLine();
-//        int goals = Integer.parseInt(totalGoals);
-//
-//        double ppg;
-//        ppg = (double) goals / totalGames;
-////        System.out.println(playerName + " has a goals per game average of "+ ppg + ", where "+ playerName+" is the player's name and "+ ppg+" is the calculated average.");   // Test with full decimal places(34.8 = 0.23529411764705882)
-//        System.out.printf( playerName + " has a goals per game average of %.2f, where "+ playerName+" is the player's name and %.2f is the calculated average.", ppg, ppg);
+        Scanner p = new Scanner(System.in);
+        System.out.println("Enter player name: ");
+        String playerName = p.nextLine();
+        System.out.println("Enter total games played: ");
+        String totalGamesPlayed = p.nextLine();
+        int totalGames = Integer.parseInt(totalGamesPlayed);
+        System.out.println("Enter the total goals: ");
+        String totalGoals = p.nextLine();
+        int goals = Integer.parseInt(totalGoals);
+
+        double ppg;
+        ppg = (double) goals / totalGames;
+//        System.out.println(playerName + " has a goals per game average of "+ ppg + ", where "+ playerName+" is the player's name and "+ ppg+" is the calculated average.");   // Test with full decimal places(34.8 = 0.23529411764705882)
+        System.out.printf( playerName + " has a goals per game average of %.2f, where "+ playerName+" is the player's name and %.2f is the calculated average.", ppg, ppg);
 
 //        Question 5:
         String numM = "15";
