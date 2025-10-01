ff9790a6f8bb86628cb297f2ddfc8c955b35504d
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-09-30 23:56:41 -0400

    Homework 3: Question 1 adding comments

--- src/PinsoeGustaveHW3.java
+++ src/PinsoeGustaveHW3.java
@@ -1,10 +1,14 @@
 public class PinsoeGustaveHW3 {
     public static void main(String[] args) {
+
+//      Question 1:
         //    Create the startYear Variable
         int startYear = 2005;
         int endYear = startYear + 20;
 
+//        FOR Loop to get us 20 years
         for (int x = startYear; x <= endYear; x++){
+//            Condition to print year born in and start at Duquesne
             if (x == 2005) {
                 System.out.println("I was born in this year");
             } else if (x == 2025) {
