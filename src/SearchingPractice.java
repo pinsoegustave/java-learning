1ac1b2b5d7373cf0754494a4406086ea086e6585
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-10-30 14:11:30 -0400

    java-learning: Searching practice in two-dimensional

--- src/SearchingPractice.java
+++ src/SearchingPractice.java
@@ -3,6 +3,8 @@ public class SearchingPractice {
 //        Create arrays
         int[] numbers = {89, 6, 47, 55, 2, 144};
         int desiredNumber = 55;
+        String[][]  books= {{"b1", "b2", "b3"}, {"b4", "b5", "b6"},{"b7", "b8", "b9"}};
+        String desiredBook  = "b3";
 
 //        Linear search
         int foundPos = -1;
@@ -19,6 +21,32 @@ public class SearchingPractice {
         }
         else {
             System.out.println("Found at pos"+ foundPos);
+        }
+
+//        Searching a two-dimensional array
+        int shelfIndex = -1;
+        int bookIndex = -1;
+
+//        Loop through the shelves
+        for (int s = 0; s < 3; s++) {
+//
+            for (int b=0; b < 3; b++) {
+//                See if this is the book
+                if (books[s][b].equals(desiredBook)) {
+                    shelfIndex = s;
+                    bookIndex = b;
+                    break;
+                }
+            }
+        }
+
+        if (shelfIndex == -1) {
+            System.out.println("Book not found");
+        }
+        else {
+            System.out.println("Book found ");
+            System.out.println("Shelf: "+ shelfIndex);
+            System.out.println("Book #:"+ bookIndex);
         }
     }
 }
