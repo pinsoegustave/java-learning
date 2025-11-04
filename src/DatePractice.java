a0577dc366d1b878074e78d24c1ccbab427be0c7
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-04 13:53:21 -0500

    java-learning: More Date practices

--- src/DatePractice.java
+++ src/DatePractice.java
@@ -1,8 +1,16 @@
+import java.util.Calendar;
 import java.util.Date;
 
 public class DatePractice {
     public static void main(String[] args) {
         Date myDate = new Date();
         System.out.println(myDate);
+
+//        New and improved
+        Calendar c = Calendar.getInstance();
+        c.set(2025, 11, 10, 54, 30);
+
+        Date cDate = c.getTime();
+        System.out.println(cDate);
     }
 }
