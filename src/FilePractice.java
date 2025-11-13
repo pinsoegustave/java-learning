8ee7334aac0bac1cbeb1223669a36d60c11fbf64
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-13 14:07:59 -0500

    java-learning: read data from a file

--- src/FilePractice.java
+++ src/FilePractice.java
@@ -1,6 +1,8 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FilePractice {
 
@@ -9,19 +11,38 @@ public class FilePractice {
         PrintWriter outputStream = null;
 
 //        Open the file
+//        try {
+//            outputStream = new PrintWriter(new FileOutputStream("MyFirstFile.txt"));
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Error opening file for write");
+//            System.exit(1);
+//        }
+
+//        Write to the file
+//        outputStream.println("Alice");
+//        outputStream.println("BoboQ");
+//
+//        Close the file
+//        outputStream.close();
+
+//        Read from the file
+        Scanner inputStream = null;
+
         try {
-            outputStream = new PrintWriter(new FileOutputStream("MyFirstFile.txt"));
+            inputStream = new Scanner(new FileInputStream("MyFirstFile.txt"));
+
         } catch (FileNotFoundException e) {
-            System.out.println("Error opening file for write");
+            System.out.println("Error opening file for read");
             System.exit(1);
         }
 
-//        Write to the file
-        outputStream.println("Alice");
-        outputStream.println("BoboQ");
+        String input;
+        while (inputStream.hasNextLine()) {
+            input = inputStream.nextLine();
+            System.out.println(input);
+        }
 
-//        Close the file
-        outputStream.close();
+        inputStream.close();
 
     }
 }
